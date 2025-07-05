3746f20c2e68c9b497ba4a7bec2585f4a6298cf2
Clément DE LA GENIERE <0xkrem@proton.me>
2025-07-06 01:17:02 +0200

    WIP: checking for duplicates in config

--- arch-deploy.sh
+++ arch-deploy.sh
@@ -21,7 +21,7 @@ log() {
 	local color=""
 	case "$1" in
 		e) label="[ERROR]" ; color="$red" ;;
-		x) label="[EXIT]" ; color="$yellow" ;;
+		w) label="[WARNING]" ; color="$yellow" ;;
 		d) label="[DEBUG]" ; color="$purple" ;;
 		i) label="[INFO]" ; color="$green" ;;
 	esac
@@ -66,43 +66,78 @@ EOF
 # 	readarray -t _CURRENT_ARRAY < <( printf "%s" "$value" | tr ":" "\n")
 # }
 
-arg_exists() {
+is_in_array() {
 	query="$1"
+	shift
+	array=("$@")
 
-	if (( ${#_ARGS[@]} == 0 )); then
-		return 1
-	fi
-
-	for i in "${!_ARGS[@]}"; do
-		if [[ "${_ARGS[$i]}" == "$query" ]]; then
-			return 0
-		fi
-		return 1
+	for item in "${array[@]}"; do
+		[[ "$item" == "$query" ]] && return
 	done
+	return 1
 }
 
 get_next_arg() {
 	query="$1"
 
-	for i in "${!_ARGS[@]}"; do
-		if [[ "${_ARGS[$i]}" == "$query" ]] && (( ${#_ARGS[@]} > $i + 1 )); then
+	for ((i = 0; i < ${#_ARGS[@]} - 1; i++)); do
+		if [[ "${_ARGS[$i]}" == "$query" ]]; then
 			echo "${_ARGS[$i + 1]}"
 			return
 		fi
-		return
 	done
+}
+
+check_config_syntax() {
+	local current_line=""
+	local line_nr=0
+	seen_keys=()
+
+	while IFS= read -r current_line; do
+		line_nr=$(( line_nr + 1 ))
+
+		local trimmed_line="$( echo "$current_line" | xargs)"
+
+		# skip comments and blank lines
+		if [[ "$trimmed_line" =~ ^# || -z "$trimmed_line" ]]; then
+			continue;
+		fi
+
+		# check format
+		if ! [[ "$trimmed_line" =~ ^[a-z][a-z_\ ]+=.*$ ]]; then
+			log e "Config l${line_nr}: Invalid key/value pair"
+			exit 1
+		fi
+
+		local trimmed_key="$(echo "$trimmed_line" | cut -d= -f1 | xargs)"
+		local trimmed_value="$(echo "$trimmed_line" | cut -d= -f2 | xargs)"
+
+		# check for duplicate
+		if is_in_array "$trimmed_key" "${seen_keys[@]}"; then
+			log w "Config l${line_nr}: Found duplicate key '$trimmed_key'"
+		else
+			seen_keys+=("$trimmed_key")
+		fi
+
+		if [[ "$trimmed_value" == *"::"* || "$trimmed_value" == *":" ]]; then
+			log w "Config l${line_nr}: Extra separator in value"
+		fi
+
+		# log d "Config l${line_nr}: passing | k=${trimmed_key} v=${trimmed_value}"
+
+	done < "$_CONFIG_FILE"
 }
 
 main() {
 
 	# show help
-	if arg_exists "--help" || (( ${#_ARGS[@]} == 0 )) ; then
+	if (( ${#_ARGS[@]} == 0 )) || is_in_array "--help" "${_ARGS[@]}" ; then
 		usage
 		exit 0
 	fi
 
 	# get config
-	if ! arg_exists "--config"; then
+	if ! is_in_array "--config" "${_ARGS[@]}"; then
 		log e "You must provide a config using --config"
 		exit 1
 	fi
@@ -116,9 +151,9 @@ main() {
 
 	log i "Found config file '$_CONFIG_FILE'"
 
-	checkConfig
+	check_config_syntax
 	
-	if arg_exists "--check" || arg_exists "-c"; then
+	if is_in_array "--check" "${_ARGS[@]}" || is_in_array "-c" "${_ARGS[@]}"; then
 		exit 0
 	fi
 
